e6f2c9f4f67ef4b4dd327447d7c67d4aea052f5d
pokrywa <h.pokrywka@wp.pl>
2022-05-23 12:51:57 +0200

    add basic layout

---
+++ django-project/korki/static/js/script.js
@@ -0,0 +1,27 @@
+// Initialize and add the map
+
+function initMap() {
+  // The location of Uluru
+
+  const uluru = { lat: -25.344, lng: 131.031 };
+
+  // The map, centered at Uluru
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 13,
+  });
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      position =>
+        map.setCenter({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude,
+        }),
+      err => alert(`Error (${err.code}): ${err.message}`)
+    );
+  } else {
+    alert('Geolocation is not supported by your browser.');
+  }
+}
+
+///<script  src="{% static 'js/script.js'" %}"></script>
+window.initMap = initMap;
