ba2d2ba7322f6333b9f26a721bc15e6add22b59d
pokrywa <h.pokrywka@wp.pl>
2022-06-08 23:20:25 +0200

    add registration system

--- django-project/korki/korkidb/views.py
+++ django-project/korki/korkidb/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
 
 
 def home(request):
@@ -24,3 +26,12 @@ def login_user(request):
 		return render(request, 'authenticate/login.html', {})
 
 
+def register_user(request):
+	form = CreateUserForm()
+	if request.method == "POST":
+		form = CreateUserForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('login')
+	context = {'form':form}
+	return render(request, 'authenticate/register.html', context)
