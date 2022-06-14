015122152ac3e31d8842869f006884fad05eb93d
JakubPelechowicz <kpelechowicz@gmail.com>
2022-06-15 00:25:28 +0200

    merge fixed

--- django-project/korki/korkidb/urls.py
+++ django-project/korki/korkidb/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('login_user', views.login_user, name='login'),
     path('',views.CalendarView.as_view(),name='calendar'),
-
+    path('register_user', views.register_user, name='register_user'),
 ]
