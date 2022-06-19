7f6601aa14907e06d191b0a06c8016131961fb0f
pokrywa <h.pokrywka@wp.pl>
2022-06-19 23:50:53 +0200

    add reset password

--- django-project/korki/korki/settings.py
+++ django-project/korki/korki/settings.py
@@ -131,3 +131,10 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/korkidb/login_user"
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'korkipostgreio@gmail.com'
+EMAIL_HOST_PASSWORD = 'bxkiwvutzossinsd'
--- django-project/korki/korkidb/urls.py
+++ django-project/korki/korkidb/urls.py
@@ -1,8 +1,18 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('login_user', views.login_user, name='login'),
     path('',views.CalendarView.as_view(),name='calendar'),
-    path('register_user', views.register_user, name='register_user'),
+    path('register_user/', views.register_user, name='register_user'),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(),name='reset_password_done'),
+
+    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(),name='password_reset_complete'),
+
+
 ]
